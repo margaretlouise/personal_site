cd7712ca6fe0ab103e456b9b6f484a72fd88b459
Margaret Adams <margaret@simple.com>
2021-01-19 17:37:16 -0800

    add short and long story functionality

--- resources/script.js
+++ resources/script.js
@@ -10,9 +10,23 @@ var intEngDiv = document.getElementById('int-eng-blurb');
 var custOpsDiv = document.getElementById('cust-ops-blurb');
 var ceoDiv = document.getElementById('ceo-blurb');
 
+var shortStoryBtn = document.getElementById('short-story-button');
+var longStoryBtn = document.getElementById('long-story-button');
+var eng2ShortStoryCopy = document.getElementById('eng-2-short-story');
+var eng2LongStoryCopy = document.getElementById('eng-2-long-story');
+var eng1ShortStoryCopy = document.getElementById('eng-1-short-story');
+var eng1LongStoryCopy = document.getElementById('eng-1-long-story');
+var intEngShortStoryCopy = document.getElementById('int-eng-short-story');
+var intEngLongStoryCopy = document.getElementById('int-eng-long-story');
+var custOpsShortStoryCopy = document.getElementById('cust-ops-short-story');
+var custOpsLongStoryCopy = document.getElementById('cust-ops-long-story');
+var ceoShortStoryCopy = document.getElementById('ceo-short-story');
+var ceoLongStoryCopy = document.getElementById('ceo-long-story');
+
 // lol. this is gonna be get better i promise.
+// also the short story long story buttons are broken kind of
 eng2Btn.onclick = function() {
-  eng2Btn.setAttribute('class', 'exp-btn active');
+  eng2Btn.setAttribute('class', 'exp-btn exp-btn-active');
   eng1Btn.setAttribute('class', 'exp-btn');
   intEngBtn.setAttribute('class', 'exp-btn');
   custOpsBtn.setAttribute('class', 'exp-btn');
@@ -22,11 +36,26 @@ eng2Btn.onclick = function() {
   intEngDiv.setAttribute('class', 'hidden');
   custOpsDiv.setAttribute('class', 'hidden');
   ceoDiv.setAttribute('class', 'hidden');
+  eng2ShortStoryCopy.setAttribute('class', 'visible');
+
+  longStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn');
+    eng2LongStoryCopy.setAttribute('class', 'visible');
+    eng2ShortStoryCopy.setAttribute('class', 'hidden');
+  }
+
+  shortStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    eng2LongStoryCopy.setAttribute('class', 'hidden');
+    eng2ShortStoryCopy.setAttribute('class', 'visible');
+  }
 };
 
 eng1Btn.onclick = function() {
   eng2Btn.setAttribute('class', 'exp-btn');
-  eng1Btn.setAttribute('class', 'exp-btn active');
+  eng1Btn.setAttribute('class', 'exp-btn exp-btn-active');
   intEngBtn.setAttribute('class', 'exp-btn');
   custOpsBtn.setAttribute('class', 'exp-btn');
   ceoBtn.setAttribute('class', 'exp-btn');
@@ -35,12 +64,27 @@ eng1Btn.onclick = function() {
   intEngDiv.setAttribute('class', 'hidden');
   custOpsDiv.setAttribute('class', 'hidden');
   ceoDiv.setAttribute('class', 'hidden');
+  eng1ShortStoryCopy.setAttribute('class', 'visible');
+
+  longStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn');
+    eng1LongStoryCopy.setAttribute('class', 'visible');
+    eng1ShortStoryCopy.setAttribute('class', 'hidden');
+  }
+
+  shortStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    eng1LongStoryCopy.setAttribute('class', 'hidden');
+    eng1ShortStoryCopy.setAttribute('class', 'visible');
+  }
 };
 
 intEngBtn.onclick = function() {
   eng2Btn.setAttribute('class', 'exp-btn');
   eng1Btn.setAttribute('class', 'exp-btn');
-  intEngBtn.setAttribute('class', 'exp-btn active');
+  intEngBtn.setAttribute('class', 'exp-btn exp-btn-active');
   custOpsBtn.setAttribute('class', 'exp-btn');
   ceoBtn.setAttribute('class', 'exp-btn');
   eng2Div.setAttribute('class', 'hidden');
@@ -48,19 +92,49 @@ intEngBtn.onclick = function() {
   intEngDiv.setAttribute('class', 'visible');
   custOpsDiv.setAttribute('class', 'hidden');
   ceoDiv.setAttribute('class', 'hidden');
+  intEngShortStoryCopy.setAttribute('class', 'visible');
+
+  longStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn');
+    intEngLongStoryCopy.setAttribute('class', 'visible');
+    intEngShortStoryCopy.setAttribute('class', 'hidden');
+  }
+
+  shortStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    intEngLongStoryCopy.setAttribute('class', 'hidden');
+    intEngShortStoryCopy.setAttribute('class', 'visible');
+  }
 };
 
 custOpsBtn.onclick = function() {
   eng2Btn.setAttribute('class', 'exp-btn');
   eng1Btn.setAttribute('class', 'exp-btn');
   intEngBtn.setAttribute('class', 'exp-btn');
-  custOpsBtn.setAttribute('class', 'exp-btn active');
+  custOpsBtn.setAttribute('class', 'exp-btn exp-btn-active');
   ceoBtn.setAttribute('class', 'exp-btn');
   eng2Div.setAttribute('class', 'hidden');
   eng1Div.setAttribute('class', 'hidden');
   intEngDiv.setAttribute('class', 'hidden');
   custOpsDiv.setAttribute('class', 'visible');
   ceoDiv.setAttribute('class', 'hidden');
+  custOpsShortStoryCopy.setAttribute('class', 'visible');
+
+  longStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn');
+    custOpsLongStoryCopy.setAttribute('class', 'visible');
+    custOpsShortStoryCopy.setAttribute('class', 'hidden');
+  }
+
+  shortStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    custOpsLongStoryCopy.setAttribute('class', 'hidden');
+    custOpsShortStoryCopy.setAttribute('class', 'visible');
+  }
 };
 
 ceoBtn.onclick = function() {
@@ -68,11 +142,25 @@ ceoBtn.onclick = function() {
   eng1Btn.setAttribute('class', 'exp-btn');
   intEngBtn.setAttribute('class', 'exp-btn');
   custOpsBtn.setAttribute('class', 'exp-btn');
-  ceoBtn.setAttribute('class', 'exp-btn active');
+  ceoBtn.setAttribute('class', 'exp-btn exp-btn-active');
   eng2Div.setAttribute('class', 'hidden');
   eng1Div.setAttribute('class', 'hidden');
   intEngDiv.setAttribute('class', 'hidden');
   custOpsDiv.setAttribute('class', 'hidden');
   ceoDiv.setAttribute('class', 'visible');
-};
+  ceoShortStoryCopy.setAttribute('class', 'visible');
 
+  longStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn');
+    ceoLongStoryCopy.setAttribute('class', 'visible');
+    ceoShortStoryCopy.setAttribute('class', 'hidden');
+  }
+
+  shortStoryBtn.onclick = function() {
+    longStoryBtn.setAttribute('class', 'exp-2-btn');
+    shortStoryBtn.setAttribute('class', 'exp-2-btn exp-2-btn-active');
+    ceoLongStoryCopy.setAttribute('class', 'hidden');
+    ceoShortStoryCopy.setAttribute('class', 'visible');
+  }
+};
