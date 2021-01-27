91c29036ba7c35b891f0551df1c892543a667f9b
Margaret Adams <margaret@simple.com>
2021-01-26 16:38:13 -0800

    remove freezeframe cause it aint workin right

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,3 @@
-$(function() {
-  ff = new freezeframe().freeze();
-})
-
 var eng2Btn = document.getElementById('eng-2-button');
 var eng1Btn = document.getElementById('eng-1-button');
 var intEngBtn = document.getElementById('int-eng-button');
